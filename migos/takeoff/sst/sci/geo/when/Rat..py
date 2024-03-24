faf5663183fb8c7b7b668d999c870cd40bd4c127
Ampaire Evans <145043834+Elijah991@users.noreply.github.com>
2024-03-25 00:11:13 +0300

    ya

--- migos/takeoff/sst/sci/geo/when/Rat..py
+++ migos/takeoff/sst/sci/geo/when/Rat..py
@@ -5,8 +5,24 @@
 #     print(loan_amount-Amount_paid_per_month)
 # print(r)
 
-num = int(input("Enter a number: "))
-if (num % 2) == 0:
-   print("{0} is Even".format(num))
-else:
-   print("{0} is Odd".format(num))
+# num = int(input("Enter a number: "))
+# if (num % 2) == 0:
+#    print("{0} is Even".format(num))
+# else:
+#    print("{0} is Odd".format(num))
+
+
+class book_shop:
+
+    # constructor
+    def __init__(self, title):
+        self.title = title
+
+    # Sample method
+    def book(self):
+        print('The tile of the book is', self.title)
+
+
+b = book_shop('Sandman')
+b.book()
+# The tile of the book is Sandman
